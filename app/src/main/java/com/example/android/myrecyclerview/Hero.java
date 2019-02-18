c8ecc8a9723a7916cf6327049919a585bc8fc7e9
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-02-18 10:53:58 +0700

    Implement ListView in RecyclerView

---
+++ app/src/main/java/com/example/android/myrecyclerview/Hero.java
@@ -0,0 +1,30 @@
+package com.example.android.myrecyclerview;
+
+public class Hero {
+    private String name, from, photo;
+
+    // Getter dan setter dari variable di class
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
--- app/src/main/java/com/example/android/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/android/myrecyclerview/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.android.myrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvCategory;
+
+    private ArrayList<Hero> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvCategory = findViewById(R.id.rv_category);
+        // Set item in recyclerview to be the same size
+        rvCategory.setHasFixedSize(true);
+
+        // Add data to ArrayList
+        list.addAll(HeroesData.getListData());
+        // Call method
+        showRecyclerList();
+    }
+
+    private void showRecyclerList(){
+        rvCategory.setLayoutManager(new LinearLayoutManager(this)); // LinearLayout is the type of RecyclerView
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(this);
+        listHeroAdapter.setListHero(list); // Call setter method dri ArrayList yg menampung data sbg parameter
+        rvCategory.setAdapter(listHeroAdapter); // Set adapter ke RecyclerView
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu); // Create option menu from menu file in xml
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_list:
+                break;
+            case R.id.action_grid:
+                break;
+            case R.id.action_cardview:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
